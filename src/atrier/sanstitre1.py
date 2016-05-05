b76ac9a61888a0ca1e149536a209ff967e6df512
DaphneHB <daphne.hbek@gmail.com>
2016-05-05 14:28:24 +0200

    distance ok; angle to verify; rotation robot to compute; todo=increase possible distance;getSensor value ko

--- src/atrier/sanstitre1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 13 23:37:35 2016
-
-@author: daphnehb
-"""
-
-import cv2
--- src/gestIO.py
+++ src/gestIO.py
@@ -7,16 +7,30 @@ Created on Fri Apr  8 16:28:54 2016
 
 import os,tools
 import time, datetime
+import threading
 
-def writeOutputFile(myStr):
+def writeOutputFile(myStr, directory=None, path=None, log=False):
     """
     Write the times computed
     """
-    st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%Hh%Mmin%Ssec')
-    filename = st+".txt"
-    if not os.path.exists(tools.FILE_PATH):
-        os.makedirs(tools.FILE_PATH)
-    path = tools.FILE_PATH+""+filename
-    # overriding the previous one (unexisting)
+    extension = ".txt" if not log else ".log"
+    if directory is None:
+        directory = tools.FILE_PATH if not log else tools.LOG_PATH		
+    if path is None:
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%Hh%Mmin')
+        filename = st+""+extension
+        path = directory+""+filename
+    else:
+        directory = os.path.abspath(os.path.dirname(path))
+    # creating the directory if not existing
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    # overriding the previous one (if existing)
     with open(path,'w') as monfile:
-        monfile.write(myStr+"\nFin Prise\n")
+        monfile.write(myStr+"\nEnd vizualization\n")
+
+class WriteLog(threading.Thread):
+    def __init__(self, filename):
+        threading.Thread.__init__(self)
+        self._stop = False
+        self.daemon = True
